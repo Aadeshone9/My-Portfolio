531ad46107c7dbb32e95c3cc2f989f09042c0737
Aadesh Govekar <aadeshgovekar@gmail.com>
2025-12-13 19:59:12 +0000

    make spacing between all the sections of 40 pixels

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
     <div className="bg-white">
       <Header />
       <main className="flex flex-col">
-        <div className="container mx-auto px-20 pt-[100px]">
+        <div className="container mx-auto px-20 pt-10">
             <div className="py-8">
               <SplitText 
                 text="Hi, I’m Aadesh" 
@@ -43,7 +43,7 @@ export default function Home() {
             )}
         </div>
 
-        <section className="py-24">
+        <section className="py-10">
           <div className="container mx-auto px-20 relative z-10">
             <div className="flex justify-between items-center">
                 <div>
@@ -73,7 +73,7 @@ export default function Home() {
           </div>
         </section>
 
-        <section className="py-12 container mx-auto px-20">
+        <section className="py-10 container mx-auto px-20">
           <div className="flex flex-col items-start gap-4 my-8">
             <div className="w-[104px] h-[104px] border-2 border-[#757575] rounded-lg"></div>
             <div className="w-[109px] border-2 border-[#757575] my-4"></div>
@@ -83,7 +83,7 @@ export default function Home() {
           </div>
         </section>
 
-        <section className="py-12 container mx-auto px-20">
+        <section className="py-10 container mx-auto px-20">
            <div className="flex flex-row items-center justify-between gap-8 md:gap-[297px]">
                 <div className="flex flex-col items-start gap-2 max-w-[875px]">
                     <h2 className="font-headline font-semibold text-[32px] leading-[58px] tracking-[0.02em] text-black flex items-center p-2.5">
@@ -98,7 +98,7 @@ export default function Home() {
             </div>
         </section>
 
-        <section className="bg-transparent py-24">
+        <section className="bg-transparent py-10">
             <div className="container mx-auto px-20 grid grid-cols-1 md:grid-cols-2 gap-8">
                 <SpotlightCard>
                     <div className="bg-white border border-[#BFBFBF] rounded-3xl p-8 h-full">
@@ -135,7 +135,7 @@ export default function Home() {
             </div>
         </section>
       </main>
-      <footer className="bg-[#131313] text-white">
+      <footer className="bg-[#131313] text-white mt-10">
         <div className="container mx-auto px-20 py-16">
             <div className="grid md:grid-cols-2 gap-16">
                 <div>
