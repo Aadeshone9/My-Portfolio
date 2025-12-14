b645dc9af024d11c77cfee2e538cfa40ec7a1096
Aadesh Govekar <aadeshgovekar@gmail.com>
2025-12-14 10:45:07 +0000

    apply the effect to the to the full component not the find out text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -150,17 +150,18 @@ export default function Home() {
         <Approach />
 
         <div className="w-[90%] mx-auto my-20">
-          <div className="bg-[#F2F2F2] rounded-2xl py-4 px-6 flex items-center">
-             <SplitText
-                text="Find out"
-                tag="h2"
+          <SpotlightCard>
+            <div className="bg-[#F2F2F2] rounded-2xl py-4 px-6 flex items-center">
+              <h2
                 className="font-headline font-semibold text-4xl leading-tight tracking-[0.02em] text-black"
-                textAlign="left"
-              />
-            <Button variant="secondary" className="bg-[#EBEAEB] rounded-[20px] text-black font-medium text-xl px-4 py-2 h-auto ml-2 hover:bg-black hover:text-white">
-              Why you should hire me
-            </Button>
-          </div>
+              >
+                Find out
+              </h2>
+              <Button variant="secondary" className="bg-[#EBEAEB] rounded-[20px] text-black font-medium text-xl px-4 py-2 h-auto ml-2 hover:bg-black hover:text-white">
+                Why you should hire me
+              </Button>
+            </div>
+          </SpotlightCard>
         </div>
 
       </main>
