73dca0742677919c32a8adfaf7e36e38c4e665a2
Aadesh Govekar <aadeshgovekar@gmail.com>
2025-12-15 06:50:34 +0000

    PAGE TITLE [ROJECTS IS aphabet is no vsisble

--- src/components/SplitText.tsx
+++ src/components/SplitText.tsx
@@ -142,7 +142,6 @@ const SplitText = ({
   const renderTag = () => {
     const style = {
       textAlign,
-      overflow: 'hidden',
       display: 'inline-block',
       whiteSpace: 'normal',
       wordWrap: 'break-word',
