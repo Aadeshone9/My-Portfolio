7e468b3418d0448d8bffcc54d85f706fe0299504
Aadesh Govekar <aadeshgovekar@gmail.com>
2025-12-23 05:04:01 +0000

    still increase the spacing between the lines by 24 PIXELS

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -70,7 +70,7 @@ export default function ProjectPage({ params }: { params: { slug: string } }) {
                <SplitText
                 text="Driving Loyalty & Efficiency in EV Mobility with VIDA"
                 tag="h1"
-                className="font-headline font-semibold text-4xl md:text-5xl lg:text-6xl text-center md:text-left max-w-4xl mx-auto md:mx-0"
+                className="font-headline font-semibold text-4xl md:text-5xl lg:text-6xl text-center md:text-left max-w-4xl mx-auto md:mx-0 leading-6"
                 textAlign="left"
               />
               <SplitText
@@ -359,4 +359,5 @@ export default function ProjectPage({ params }: { params: { slug: string } }) {
   );
 }
 
+    
     
