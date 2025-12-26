1125892405a3b14e790afd927c46b4c81af659c8
Aadesh Govekar <aadeshgovekar@gmail.com>
2025-12-26 18:02:35 +0000

    also remove logo from Hero Motocorp Vida card as well

--- src/components/ProjectStack.tsx
+++ src/components/ProjectStack.tsx
@@ -59,21 +59,7 @@ const ProjectCard = ({ project, i, progress, range, targetScale }: { project: an
         <Card className={`${project.bgColor} border-border rounded-2xl w-full h-full mx-auto flex flex-col overflow-hidden`}>
           <div className={`relative ${project.imageBgColor} w-full h-full rounded-lg flex items-center justify-center`}>
             {project.title === 'Hero Motocorp Vida App' ? (
-                <svg width="232" height="40" viewBox="0 0 232 40" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-[150px] h-auto md:w-[232px]">
-                    <path d="M0 40V0H9.33333V40H0Z" fill="#F36F2D"/>
-                    <path d="M26.6667 40V0H36V40H26.6667Z" fill="#231F20"/>
-                    <path d="M26.6667 24.6667V15.3333H54.6667V24.6667H26.6667Z" fill="#231F20"/>
-                    <path d="M54.6667 40V0H64V40H54.6667Z" fill="#231F20"/>
-                    <path d="M73.3333 40V0H82.6667V40H73.3333Z" fill="#F36F2D"/>
-                    <path d="M92 40V0H101.333V40H92Z" fill="#231F20"/>
-                    <path d="M128.667 40L110 0H120L138.667 40H128.667Z" fill="#231F20"/>
-                    <path d="M129.333 0L148 40H138L119.333 0H129.333Z" fill="#231F20"/>
-                    <path d="M157.333 28.16V3.52H166.667V40H157.333V28.16Z" fill="#F36F2D"/>
-                    <path d="M188.667 40V15.3333H170V40H188.667V24.6667H179.333V15.3333H188.667V40Z" fill="#231F20"/>
-                    <path d="M170 0H188.667V15.3333H170V0Z" fill="#231F20"/>
-                    <path d="M213.333 40C204 40 198.667 30.6667 198.667 20C198.667 9.33333 204 0 213.333 0C222.667 0 228 9.33333 228 20C228 30.6667 222.667 40 213.333 40ZM213.333 30.6667C218 30.6667 218.667 24.6667 218.667 20C218.667 15.3333 218 9.33333 213.333 9.33333C208.667 9.33333 208 15.3333 208 20C208 24.6667 208.667 30.6667 213.333 30.6667Z" fill="#231F20"/>
-                    <path d="M231.333 28.16V3.52H222V40H231.333V28.16Z" fill="#F36F2D"/>
-                </svg>
+                null
             ) : null }
              <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-black/30 to-transparent p-6 flex flex-col justify-end">
                 <div className="text-white">
