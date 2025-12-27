22d4290f7ea00a71e1df340690c32cc21f59e3b5
Aadesh Govekar <aadeshgovekar@gmail.com>
2025-12-27 12:48:41 +0000

    make it the animation more fast

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -116,7 +116,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'looping-scroll': 'looping-scroll 10s linear infinite',
+        'looping-scroll': 'looping-scroll 5s linear infinite',
       },
     },
   },
