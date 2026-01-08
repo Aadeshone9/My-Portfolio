296441886f087cbf3d7afda1f025d09b80798c23
Aadesh Govekar <aadeshgovekar@gmail.com>
2026-01-08 11:35:59 +0000

    chnage the backgroud colour of the logos in the project cards on the hom

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -86,7 +86,7 @@ export default {
           'muted-foreground': 'hsl(var(--footer-muted-foreground))',
         },
         'off-white': '#F8F9FA',
-        'off-white-f4': '#F4F4F4',
+        'off-white-f4': '#ECECEC',
       },
       borderRadius: {
         lg: 'var(--radius)',
